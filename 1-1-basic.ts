80865be5b873bbe260e66f9b171fa788853e7531
Jinkyu <rlawlsrb16736@gmail.com>
2025-12-14 23:29:08 +0900

    기본 타입 정리 3 (unknown, any, void, never, object)

--- 1-1-basic.ts
+++ 1-1-basic.ts
@@ -44,4 +44,46 @@
   function findUser(): number | undefined {
     return undefined;
   }
+
+  // unknown: 아직 결정되지 않은 데이터 타입
+  let notSure: unknown = 42;
+  notSure = 'hello';
+  // unknown 타입은 어떤 타입이든 할당할 수 있다.
+  // unknown 타입은 가능한 지양한다.
+
+  // any: 모든 타입을 할당할 수 있다.
+  let anything: any = 42;
+  anything = 'hello';
+  anything = true;
+  // any 타입도 가능한 지양한다.
+
+  // void: 함수에서 반환값이 없을 때 사용
+  function voidFunction(): void {
+    console.log('void');
+  }
+  voidFunction();
+
+  // never: 절대 반환하지 않는 함수
+  function throwError(message: string): never {
+    throw new Error(message);
+  }
+  // throwError('error message');
+
+  function infiniteLoop(): never {
+    while (true) {
+      console.log('infinite loop');
+    }
+  }
+  // infiniteLoop();
+
+  // object: 객체 타입
+  let personObj: object = { name: 'John', age: 30 };
+  console.log(personObj);
+  let personArray: object[] = [personObj, personObj];
+  console.log(personArray);
+  // object 타입은 원시 타입을 제외한 모든 타입을 의미한다.
+  
+  // object는 상세한 타입을 지정하는 것을 지향한다.
+  let personObj2: { name: string, age: number } = { name: 'John', age: 30 };
+  console.log(personObj2);
 }
