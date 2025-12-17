9d598b5a01571e8a9c9b19a36e836a5249d2e954
Jinkyu <rlawlsrb16736@gmail.com>
2025-12-17 17:17:46 +0900

    typescript 좌표 게임

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -2,13 +2,12 @@
  * Let's make a calculator 🧮
  */
 
-type Methods = 'add' | 'substract' | 'multiply' | 'divide' | 'remainder';
-type Calculate = number | Error;
-function calculate(methods:Methods, num1:number, num2:number): Calculate{
+type Methods = 'add' | 'subtract' | 'multiply' | 'divide' | 'remainder';
+function calculate(methods:Methods, num1:number, num2:number): number{
   switch(methods){
     case 'add':
       return num1 + num2;
-    case 'substract':
+    case 'subtract':
       return num1 - num2;
     case 'multiply' :
       return num1 * num2;
@@ -17,11 +16,11 @@ function calculate(methods:Methods, num1:number, num2:number): Calculate{
     case 'remainder' :
       return num1 % num2;
     default: 
-      throw Error('알 수 없는 methods입니다.');
+      throw new Error('알 수 없는 methods입니다.');
   }
 }
 console.log(calculate('add', 1, 3)); // 4
-console.log(calculate('substract', 3, 1)); // 2
+console.log(calculate('subtract', 3, 1)); // 2
 console.log(calculate('multiply', 4, 2)); // 8
 console.log(calculate('divide', 4, 2)); // 2
 console.log(calculate('remainder', 5, 2)); // 1
--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,6 +1,34 @@
 /**
  * Let's make a game 🕹
  */
+
+type Position = {
+  x: number;
+  y: number;
+}
+
+type Direction = 'up' | 'down' | 'left' | 'right';
+
+const position: Position = { x: 0, y: 0 };
+
+function move(direction:Direction):void{
+  switch(direction){
+    case 'up':
+      position.y += 1;
+      break
+    case 'down':
+      position.y -= 1;
+      break
+    case 'left':
+      position.x -= 1;
+      break
+    case 'right':
+      position.x += 1;
+      break
+    default:
+      throw new Error(`${direction}은 알 수 없는 방향입니다`);
+  }
+}
 console.log(position); // { x: 0, y: 0}
 move('up');
 console.log(position); // { x: 0, y: 1}
